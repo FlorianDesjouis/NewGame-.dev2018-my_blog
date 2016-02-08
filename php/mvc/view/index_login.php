ca4c6a07f33f87b17eb8f5632ed90d00278e5367
urnotwrex <gael.marguiller@supinternet.fr>
2016-02-08 16:44:38 +0100

    update01
    Ajout de la page d'inscription
    Ajout de la page de login
    Ajout du controler de login
    Erreur 404 quand on essayer de se connecter

---
+++ php/mvc/view/index_login.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wrex
+ * Date: 06/02/2016
+ * Time: 13:54
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+Merci de vous connecter :
+<form method="POST" action="controler_login.php">
+    Login : <input type="text" name="username"><br>
+    Password : <input type="text" name="password"><br>
+    <input type="submit" value="Log in">
+</form>
+</body>
+</html>
---
+++ php/mvc/view/index_registration.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wrex
+ * Date: 08/02/2016
+ * Time: 16:19
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style src="reset.css" rel="stylesheet"></style>
+    <style src="style.css" rel="stylesheet"></style>
+    <script src="#"></script>
+    <script src="#"></script>
+    <script src="#"></script>
+    <title>New game+ - inscription</title>
+</head>
+<body>
+<form method="post" action="#">
+    <input type="text" name="pseudo" placeholder="Pseudo">Vôtre speudo<br>
+    <input type="text" name="firstname" placeholder="Prénom">Vôtre prénom<br>
+    <input type="text" name="lastname" placeholder="Nom">Vôtre nom<br>
+    <input type="email" name="email" placeholder="E-mail">Vôtre adresse mail<br>
+    <input type="email" name="check-email" placeholder="Verif. e-mail">Vérification e-mail<br>
+    <input type="password" name="password" placeholder="Mot de passe">Vôtre mot de passe<br>
+    <input type="password" name="check-password" placeholder="Vérif. mot de passe">Vérification mot de passe<br>
+    <input type="submit" value="Envoyer">
+</form>
+</body>
+</html>
