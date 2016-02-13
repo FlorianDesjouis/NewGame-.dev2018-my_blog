6c1c71098e7e0debc93d468dc096e2530d35b2ba
FlorianDesjouis <desjouisflorian@gmail.com>
2016-02-13 12:32:12 +0100

    php controllerArticles and modelArticles

--- php/mvc/controller/Articles/ControllerArticles.php
+++ php/mvc/controller/Articles/ControllerArticles.php
@@ -6,25 +6,32 @@ class controllerArticle {
 
         if(isset($_POST['title']) && isset($_POST['content'])){
 
-            require "../../model/articles/modelArticle.php";
+            require_once "../../model/articles/modelArticle.php";
             $model = new modelArticle();
-            $model->NewArticle($_POST['title'], $_POST['content'], date('y.m.d'));
+            $model->NewArticle($_POST['title'], $_POST['content'], date('Y.m.d'));
 
+        }else{
+            require_once "../../view/articles/create.php";
         };
 
-
     }
-    public function readArticle(){
+    public function readArticle($id){
 
+        if(isset($id)){
+            require_once "../../view/articles/read.php";
+        }
 
     }
     public function updateArticle (){
 
         if(isset($_POST['title']) && isset($_POST['content'])){
 
-            require "../../articles/modelArticle.php";
+            require_once "../../model/articles/modelArticle.php";
             $model = new modelArticle();
-            $model->UpdatetArticle($_POST['title'], $_POST['content'], date('y.m.d'));
+            $model->UpdateArticle($_POST['title'], $_POST['content'], date('Y.m.d'));
+        }else{
+
+            require_once "../../view/articles/update.php";
         }
     }
 
--- php/mvc/model/Articles/ModelArticle.php
+++ php/mvc/model/Articles/ModelArticle.php
@@ -1,2 +1,39 @@
 <?php
+    require_once("../config/config.php");
+class modelArticle{
+    public function createArticle($titre, $article, $date){
+        global $pdo;
+        $database = $pdo->prepare("INSERT_INTO `articles`(titre, article, date) VALUES (:titre, :article, :date)");
+        $database->bindParam('titre',$titre);
+        $database->bindParam('article',$article);
+        $database->bindParam('date',$date);
+        $database->execute();
+    }
 
+    public function readArticle($id){
+        global $pdo;
+        $database = $pdo->prepare("SELECT * FROM `articles` WHERE `id` = :id");
+        $database->bindParam("id",$id);
+        $database->execute();
+        return $database->fetch();
+    }
+    public function updateArticle($titre, $article, $date){
+        global $pdo;
+        $database = $pdo->prepare("UPDATE `articles` SET `titre` = :titre, `article` = :article, `date` = :date");
+        $database->bindParam('titre',$titre);
+        $database->bindParam('article',$article);
+        $database->bindParam('date',$date);
+        $database->execute();
+    }
+    public function deleteArticle($id){
+        global $pdo;
+        $database = $pdo->prepare("DELETE FROM `articles` WHERE `id` = :id");
+        $database->bindParam('id',$id);
+        $database->execute();
+    }
+    public function  getAllArticle(){
+        global $pdo;
+        $database = $pdo->query("SELECT * FROM `articles`");
+        return $database;
+    }
+}
