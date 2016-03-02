6a1dceb7adff0aa7d6daff95b944bca77363489e
FlorianDesjouis <desjouisflorian@gmail.com>
2016-03-02 23:14:18 +0100

    finished css 1441px home.php + work on MVC

--- php/mvc/view/articles/update.php
+++ php/mvc/view/articles/update.php
@@ -2,12 +2,12 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Nouvel Article</title>
+    <title>Modifier Article</title>
 </head>
 <body>
     <form method="post" action="articles/update">
-        <input type="text" name="title" placeholder="Titre de l'article"><br><br>
-        <textarea name="content"  placeholder="Contenu de l'article"></textarea><br>
+        <input type="text" name="title" placeholder="<?=?>"><br><br>
+        <textarea name="content"  placeholder="<?=?>"></textarea><br>
         <input type="button" value="Choisissez un fichier">&nbsp;&nbsp;
         Date du jour : <?= date("d.m.y") ?><br><br>
 <input type="button" value="Envoyer">
