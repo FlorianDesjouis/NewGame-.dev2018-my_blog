5f7061589ed8ade4cc7719c1a244cbafa5cc92d6
urnotwrex <gael.marguiller@supinternet.fr>
2016-03-02 23:42:56 +0100

    page d'inscriptions
    css page d'inscriptions

--- php/mvc/view/index_registration.php
+++ php/mvc/view/index_registration.php
@@ -4,23 +4,36 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <style src="reset.css" rel="stylesheet"></style>
-    <style src="style.css" rel="stylesheet"></style>
-    <script src="#"></script>
-    <script src="#"></script>
-    <script src="#"></script>
+    <meta name="viewport" content="width=device-width, initial-scale= 1.0, minimum-scale= 0.20">
+    <link href="../public/css/reset.css" rel="stylesheet">
+    <link href="../public/css/style.css" rel="stylesheet">
+    <script src="../public/js/jquery-2.2.0.min.js"></script>
+    <script src="../public/js/script.js"></script>
     <title>New game+ - inscription</title>
 </head>
 <body>
-<form method="post" action="#">
-    <input type="text" name="pseudo" placeholder="Pseudo">Votre speudo<br>
-    <input type="text" name="firstname" placeholder="Prénom">Votre prénom<br>
-    <input type="text" name="lastname" placeholder="Nom">Votre nom<br>
-    <input type="email" name="email" placeholder="E-mail">Votre adresse mail<br>
-    <input type="email" name="check-email" placeholder="Verif. e-mail">Vérification e-mail<br>
-    <input type="password" name="password" placeholder="Mot de passe">Votre mot de passe<br>
-    <input type="password" name="check-password" placeholder="Vérif. mot de passe">Vérification mot de passe<br>
-    <input type="submit" value="Envoyer">
-</form>
+<div class="mainConteiner" role="main">
+    <header class="header">
+        <div class="blockLogo"><img src="../public/asset/img/logo_blog.png" alt="logo_blog"></div>
+        <a href="#" class="headerIcon" id="headerIcon"></a>
+        <nav class="menu">
+            <a href="#">Acceuil</a>
+            <a href="#">Article</a>
+            <a href="#">Image</a>
+            <a href="#">Vidéos</a>
+        </nav>
+    </header>
+    <div class="blockForm">
+        <form method="post" action="#" class="registerForm">
+            Votre pseudo <br><input type="text" name="pseudo" placeholder="Pseudo" class="field"><br><br>
+            Votre prénom <br><input type="text" name="firstname" placeholder="Prénom" class="field"><br><br>
+            Votre nom <br><input type="text" name="lastname" placeholder="Nom" class="field"><br><br>
+            Votre adresse mail <br><input type="email" name="email" placeholder="E-mail" class="field"><br><br>
+            Vérification e-mail <br><input type="email" name="check-email" placeholder="Verif. e-mail" class="field"><br><br>
+            Votre mot de passe <br><input type="password" name="password" placeholder="Mot de passe" class="field"><br><br>
+            Vérification mot de passe <br><input type="password" name="check-password" placeholder="Vérif. mot de passe" class="field"><br><br>
+            <input type="submit" value="Envoyer" class="field">
+        </form>
+    </div>
 </body>
 </html>
