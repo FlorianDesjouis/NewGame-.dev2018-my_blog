9f0ba81700141c3883f0823d56e3887b117f1967
urnotwrex <gael.marguiller@supinternet.fr>
2016-03-03 22:07:33 +0100

    page login
    css(mobile/tablette)

--- php/mvc/view/index_login.php
+++ php/mvc/view/index_login.php
@@ -11,14 +11,49 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale= 1.0, minimum-scale= 0.20">
+    <link href="../public/css/reset.css" rel="stylesheet">
+    <link href="../public/css/style.css" rel="stylesheet">
+    <script src="../public/js/jquery-2.2.0.min.js"></script>
+    <script src="../public/js/jquery-ui.min.js"></script>
+    <script src="../public/js/script.js"></script>
+    <title>New game+</title>
 </head>
 <body>
-Merci de vous connecter :
-<form method="POST" action="../controler/controler_login.php">
-    Login : <input type="text" name="username"><br>
-    Password : <input type="text" name="password"><br>
-    <input type="submit" value="Login">
-</form>
+<div class="mainConteiner">
+    <header class="header">
+        <div class="blockLogo"><img src="../public/asset/img/logo_blog.png" alt="logo_blog"></div>
+        <a href="#" class="headerIcon" id="headerIcon"></a>
+        <nav class="menu">
+            <a href="#">Accueil</a>
+            <a href="#">Article</a>
+            <a href="#">Image</a>
+            <a href="#">Connexion</a>
+        </nav>
+        <div class="desktopMenu">
+            <a href="#">Accueil</a>
+            <a href="#">Image</a>
+            <a href="#">Profil</a>
+            <a href="#">Connexion</a></div>
+        <form class="login">
+            E-mail : <input type="email" name="userEmail" id="userEmail"><label for="userEmail"></label><br>
+            Password : <input type="password" name="userPassword" id="userPassword"><label
+                for="userPassword"></label><br>
+            <input type="submit" value="Login">
+        </form>
+    </header>
+    <div class="blockLogin">
+        <form class="loginForm">
+            Merci de vous connecter :<br><br>
+            E-mail : <input type="email" name="userEmail" id="userEmail" class="fieldLogin"><label for="userEmail"></label><br>
+            Password : <input type="password" name="userPassword" id="userPassword" class="fieldLogin"><label
+                for="userPassword"></label><br>
+            <input type="submit" value="Login" class="fieldLogin">
+        </form>
+    </div>
+    <footer class="footer">
+        2016 © New game+ - Tous droits reservés
+    </footer>
+</div>
 </body>
 </html>
