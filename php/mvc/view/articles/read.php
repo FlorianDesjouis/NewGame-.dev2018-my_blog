1a9d8ec113eb304063081645fb07bd4728435c83
urnotwrex <gael.marguiller@supinternet.fr>
2016-03-03 01:22:11 +0100

    read article
    css(mobile/tablette)

--- php/mvc/view/articles/read.php
+++ php/mvc/view/articles/read.php
@@ -1,2 +1,154 @@
 <?php
-echo 'bzefinzmoefzieflzef';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale= 1.0, minimum-scale= 0.20">
+    <link href="../../public/css/reset.css" rel="stylesheet">
+    <link href="../../public/css/style.css" rel="stylesheet">
+    <script src="../../public/js/jquery-2.2.0.min.js"></script>
+    <script src="../../public/js/script.js"></script>
+    <title>New game+</title>
+</head>
+<body>
+<div class="mainConteiner" role="main">
+    <header class="header">
+        <div class="blockLogo"><img src="../../public/asset/img/logo_blog.png" alt="logo_blog"></div>
+        <a href="#" class="headerIcon" id="headerIcon"></a>
+        <nav class="menu">
+            <a href="#">Acceuil</a>
+            <a href="#">Article</a>
+            <a href="#">Image</a>
+            <a href="#">Vidéos</a>
+        </nav>
+        <form class="login">
+            E-mail : <input type="email" name="userEmail" id="userEmail"><label for="userEmail"></label><br>
+            Password : <input type="password" name="userPassword" id="userPassword"><label
+                for="userPassword"></label><br>
+            <input type="submit" value="Login">
+        </form>
+    </header>
+    <section class="section">
+        <article class="article">
+            <h2 class="title">Salut</h2>
+            <p class="blockText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur scelerisque
+                egestas. Aliquam
+                elit sapien, mollis in mattis sit amet, pharetra at lorem. Sed placerat ac magna eget vulputate. Sed
+                varius
+                volutpat arcu sed maximus. Aenean fringilla efficitur justo, eget dictum nunc. Maecenas at mattis augue.
+                Quisque sed ligula non quam dictum ornare. Morbi placerat tellus id eros aliquam interdum. Aenean
+                lobortis
+                justo a neque sodales, vel aliquet magna euismod. Integer placerat porta nisl, eget finibus tortor
+                accumsan
+                eu. Quisque sit amet velit ultricies, ornare lectus ac, faucibus felis. Sed eget nisi ultricies,
+                tristique
+                enim eu, aliquet est. Nullam malesuada vehicula porttitor. Nunc pretium magna eget congue dapibus.
+                Praesent
+                nibh diam, malesuada mattis dignissim at, accumsan ut nulla.
+
+                Aliquam ante magna, pharetra in eros vel, feugiat vulputate nisl. Aliquam convallis neque ac velit
+                euismod
+                efficitur. Praesent malesuada metus quis ex consectetur dictum. Nam consectetur nunc nec suscipit
+                euismod.
+                Mauris sit amet quam commodo, efficitur sem vel, molestie lorem. Morbi tellus nisl, auctor a nisl eu,
+                eleifend malesuada sem. Vivamus justo dolor, finibus efficitur est et, tincidunt sollicitudin tellus.
+            </p>
+            <div class="blockPick"><img src="../../public/asset/img/pic1.png"></div>
+            <p class="blockText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur scelerisque
+                egestas. Aliquam
+                elit sapien, mollis in mattis sit amet, pharetra at lorem. Sed placerat ac magna eget vulputate. Sed
+                varius
+                volutpat arcu sed maximus. Aenean fringilla efficitur justo, eget dictum nunc. Maecenas at mattis augue.
+                Quisque sed ligula non quam dictum ornare. Morbi placerat tellus id eros aliquam interdum. Aenean
+                lobortis
+                justo a neque sodales, vel aliquet magna euismod. Integer placerat porta nisl, eget finibus tortor
+                accumsan
+                eu. Quisque sit amet velit ultricies, ornare lectus ac, faucibus felis. Sed eget nisi ultricies,
+                tristique
+                enim eu, aliquet est. Nullam malesuada vehicula porttitor. Nunc pretium magna eget congue dapibus.
+                Praesent
+                nibh diam, malesuada mattis dignissim at, accumsan ut nulla.
+
+                Aliquam ante magna, pharetra in eros vel, feugiat vulputate nisl. Aliquam convallis neque ac velit
+                euismod
+                efficitur. Praesent malesuada metus quis ex consectetur dictum. Nam consectetur nunc nec suscipit
+                euismod.
+                Mauris sit amet quam commodo, efficitur sem vel, molestie lorem. Morbi tellus nisl, auctor a nisl eu,
+                eleifend malesuada sem. Vivamus justo dolor, finibus efficitur est et, tincidunt sollicitudin tellus.
+            </p>
+            <p class="blockText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur scelerisque
+                egestas. Aliquam
+                elit sapien, mollis in mattis sit amet, pharetra at lorem. Sed placerat ac magna eget vulputate. Sed
+                varius
+                volutpat arcu sed maximus. Aenean fringilla efficitur justo, eget dictum nunc. Maecenas at mattis augue.
+                Quisque sed ligula non quam dictum ornare. Morbi placerat tellus id eros aliquam interdum. Aenean
+                lobortis
+                justo a neque sodales, vel aliquet magna euismod. Integer placerat porta nisl, eget finibus tortor
+                accumsan
+                eu. Quisque sit amet velit ultricies, ornare lectus ac, faucibus felis. Sed eget nisi ultricies,
+                tristique
+                enim eu, aliquet est. Nullam malesuada vehicula porttitor. Nunc pretium magna eget congue dapibus.
+                Praesent
+                nibh diam, malesuada mattis dignissim at, accumsan ut nulla.
+
+                Aliquam ante magna, pharetra in eros vel, feugiat vulputate nisl. Aliquam convallis neque ac velit
+                euismod
+                efficitur. Praesent malesuada metus quis ex consectetur dictum. Nam consectetur nunc nec suscipit
+                euismod.
+                Mauris sit amet quam commodo, efficitur sem vel, molestie lorem. Morbi tellus nisl, auctor a nisl eu,
+                eleifend malesuada sem. Vivamus justo dolor, finibus efficitur est et, tincidunt sollicitudin tellus.
+            </p>
+            <div class="blockPick"><img src="../../public/asset/img/pic2.png"></div>
+            <p class="blockText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur scelerisque
+                egestas. Aliquam
+                elit sapien, mollis in mattis sit amet, pharetra at lorem. Sed placerat ac magna eget vulputate. Sed
+                varius
+                volutpat arcu sed maximus. Aenean fringilla efficitur justo, eget dictum nunc. Maecenas at mattis augue.
+                Quisque sed ligula non quam dictum ornare. Morbi placerat tellus id eros aliquam interdum. Aenean
+                lobortis
+                justo a neque sodales, vel aliquet magna euismod. Integer placerat porta nisl, eget finibus tortor
+                accumsan
+                eu. Quisque sit amet velit ultricies, ornare lectus ac, faucibus felis. Sed eget nisi ultricies,
+                tristique
+                enim eu, aliquet est. Nullam malesuada vehicula porttitor. Nunc pretium magna eget congue dapibus.
+                Praesent
+                nibh diam, malesuada mattis dignissim at, accumsan ut nulla.
+
+                Aliquam ante magna, pharetra in eros vel, feugiat vulputate nisl. Aliquam convallis neque ac velit
+                euismod
+                efficitur. Praesent malesuada metus quis ex consectetur dictum. Nam consectetur nunc nec suscipit
+                euismod.
+                Mauris sit amet quam commodo, efficitur sem vel, molestie lorem. Morbi tellus nisl, auctor a nisl eu,
+                eleifend malesuada sem. Vivamus justo dolor, finibus efficitur est et, tincidunt sollicitudin tellus.
+            </p>
+            <p class="blockText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur scelerisque
+                egestas. Aliquam
+                elit sapien, mollis in mattis sit amet, pharetra at lorem. Sed placerat ac magna eget vulputate. Sed
+                varius
+                volutpat arcu sed maximus. Aenean fringilla efficitur justo, eget dictum nunc. Maecenas at mattis augue.
+                Quisque sed ligula non quam dictum ornare. Morbi placerat tellus id eros aliquam interdum. Aenean
+                lobortis
+                justo a neque sodales, vel aliquet magna euismod. Integer placerat porta nisl, eget finibus tortor
+                accumsan
+                eu. Quisque sit amet velit ultricies, ornare lectus ac, faucibus felis. Sed eget nisi ultricies,
+                tristique
+                enim eu, aliquet est. Nullam malesuada vehicula porttitor. Nunc pretium magna eget congue dapibus.
+                Praesent
+                nibh diam, malesuada mattis dignissim at, accumsan ut nulla.
+
+                Aliquam ante magna, pharetra in eros vel, feugiat vulputate nisl. Aliquam convallis neque ac velit
+                euismod
+                efficitur. Praesent malesuada metus quis ex consectetur dictum. Nam consectetur nunc nec suscipit
+                euismod.
+                Mauris sit amet quam commodo, efficitur sem vel, molestie lorem. Morbi tellus nisl, auctor a nisl eu,
+                eleifend malesuada sem. Vivamus justo dolor, finibus efficitur est et, tincidunt sollicitudin tellus.
+            </p>
+        </article>
+    </section>
+    <footer class="footer">
+        2016 © New game+ - Tous droits reservés
+    </footer>
+</div>
+</body>
+</html>
