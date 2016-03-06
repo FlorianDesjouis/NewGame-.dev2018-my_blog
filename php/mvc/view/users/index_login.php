53a9ae0b45b9ddce3aec68f62897dc2fe2836eb8
urnotwrex <gael.marguiller@supinternet.fr>
2016-03-06 16:33:56 +0100

    modif acces login page and register page

--- php/mvc/view/users/index_login.php
+++ php/mvc/view/users/index_login.php
@@ -12,17 +12,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale= 1.0, minimum-scale= 0.20">
-    <link href="../public/css/reset.css" rel="stylesheet">
-    <link href="../public/css/style.css" rel="stylesheet">
-    <script src="../public/js/jquery-2.2.0.min.js"></script>
-    <script src="../public/js/jquery-ui.min.js"></script>
-    <script src="../public/js/script.js"></script>
+    <link href="../../public/css/reset.css" rel="stylesheet">
+    <link href="../../public/css/style.css" rel="stylesheet">
+    <script src="../../public/js/jquery-2.2.0.min.js"></script>
+    <script src="../../public/js/jquery-ui.min.js"></script>
+    <script src="../../controller/Users/loginScript.js"></script>
+    <script src="../../public/js/script.js"></script>
     <title>New game+</title>
 </head>
 <body>
 <div class="mainConteiner">
     <header class="header">
-        <div class="blockLogo"><img src="../public/asset/img/logo_blog.png" alt="logo_blog"></div>
+        <div class="blockLogo"><img src="../../public/asset/img/logo_blog.png" alt="logo_blog"></div>
         <a href="#" class="headerIcon"></a>
         <a href="#" class="hideIcon">
             <hr id="croix1">
--- php/mvc/view/users/index_registration.php
+++ php/mvc/view/users/index_registration.php
@@ -5,10 +5,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale= 1.0, minimum-scale= 0.20">
-    <link href="../public/css/reset.css" rel="stylesheet">
-    <link href="../public/css/style.css" rel="stylesheet">
-    <script src="../public/js/jquery-2.2.0.min.js"></script>
-    <script src="../public/js/script.js"></script>
+    <link href="../../public/css/reset.css" rel="stylesheet">
+    <link href="../../public/css/style.css" rel="stylesheet">
+    <script src="../../public/js/jquery-2.2.0.min.js"></script>
+    <script src="../../public/js/jquery-ui.min.js"></script>
+    <script src="../../controller/Users/registerScript.js"></script>
+    <script src="../../public/js/script.js"></script>
     <title>New game+ - inscription</title>
 </head>
 <body>
