0389ced35802c8243eb6c89a6c7d8979a8c2cdb7
AlexisAfonso <alexis.afonso@supinternet.fr>
2016-03-06 20:11:42 +0100

    add toTheTop plug in button

--- php/mvc/view/index_home.php
+++ php/mvc/view/index_home.php
@@ -8,11 +8,18 @@
     <script src="js/jquery-2.2.0.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
     <script src="js/script.js"></script>
+    <script src="../../public/js/optional_plugin_slide.js"></script>
+    <script src="../../public/js/checkPosition.js"></script>
     <title>New game+</title>
+    <script>
+        $(function(){
+            $('#header').myPlugin({time:1000});
+        });
+    </script>
 </head>
 <body>
 <div class="mainConteiner" role="main">
-    <header class="header">
+    <header class="header" id="header">
         <div class="blockLogo"><img src="asset/img/logo_blog.png" alt="logo_blog"></div>
         <a href="#" class="headerIcon" id="headerIcon"></a>
         <nav class="menu">
@@ -134,5 +141,6 @@
         2016 © New game+ - Tous droits reservés
     </footer>
 </div>
+<a href="#header"><img src="../../public/asset/img/logo_blog.png" id="toTheTop" alt="toTheTop button"></a>
 </body>
 </html>
